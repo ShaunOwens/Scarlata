0feadc617e0a58630da0efe9503eb25ff10cc910
ShaunOwens <39343652+ShaunOwens@users.noreply.github.com>
2018-05-21 10:46:22 -0500

    Add files via upload

--- wget_loops.py
+++ wget_loops.py
@@ -3,25 +3,25 @@ import matplotlib
 from astropy.io import fits
 from astropy.io.votable import parse
 import subprocess
-import shlex
+import sys
+import os
 
 #Fits file input and value labeling#
-mass = fits.open("Data/SDSS/Fits/Mass/totlgm_dr7_v5_2.fit")
-ra_dec = fits.open("Data/SDSS/Fits/Ra_Dec/gal_info_dr7_v5_2.fit")
+mass = fits.open("B:/Scarlata/Data/SDSS/Fits/Mass/totlgm_dr7_v5_2.fit")
+ra_dec = fits.open("B:/Scarlata/Data/SDSS/Fits/Ra_Dec/gal_info_dr7_v5_2.fit")
 #mass.info()
 #ra_dec.info()
 info = ra_dec[1].columns
 data = ra_dec[1].data
 mass.close()
 ra_dec.close()
-
 #Loop for getting fits files from HST#
 #927552
-for i in range(0,927552):
+os.chdir("B:/Scarlata/Data/Hubble/VO")
+for i in range(92961,927552):
     values = data[i]
     ra = values[4]
     dec = values[5]
     name = i
-    #Create invironment for files.
-    #Input shell script and parameters.
-    
+    subprocess.call(["B:/wget.exe","-O",str(name)+"_ra("+str(ra)+")_dec("+str(dec)+").csv","http://hla.stsci.edu/cgi-bin/hlaSIAP.cgi?POS="+str(ra)+","+str(dec)+"&SIZE=0.05&inst=WFC3&format=image/fits,application/tar,text/html"],shell=True)
+os.chdir("B:/Scarlata/Code")
